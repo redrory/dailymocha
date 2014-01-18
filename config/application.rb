65752f2ac83c324ed02caf4f665aa05ad5f7f377
Rory Walker <redrory@gmail.com>
2014-01-17 20:11:20 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,10 @@ module Dailymocha
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    config.assets.initialize_on_precompile = false
+
+    config.assets.precompile += %w( jumbotron-narrow.css  )
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
